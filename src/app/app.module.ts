c12676630f7d6adf3cf89b833580027b3c8ebba4
Michael Giambalvo <heathkit@google.com>
2017-07-13 03:06:34 -0700

    Fixing header, adding table component scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IceCreamTableComponent } from './ice-cream-table/ice-cream-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IceCreamTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
