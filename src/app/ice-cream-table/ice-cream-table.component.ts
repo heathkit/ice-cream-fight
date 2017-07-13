7ed05af24217060c514000616328217674cc785c
Michael Giambalvo <heathkit@google.com>
2017-07-13 14:49:04 -0700

    Add animations to flavor table

--- src/app/ice-cream-table/ice-cream-table.component.ts
+++ src/app/ice-cream-table/ice-cream-table.component.ts
@@ -4,6 +4,14 @@ import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 // Needed to use on the flavor list!
 import 'rxjs/add/operator/map';
@@ -11,7 +19,16 @@ import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-ice-cream-table',
   templateUrl: './ice-cream-table.component.html',
-  styleUrls: ['./ice-cream-table.component.css']
+  styleUrls: ['./ice-cream-table.component.css'],
+  animations: [
+      trigger('pulse', [
+        state('in', style({background: 'white'})),
+        transition('void => *', [
+          style({background: 'grey'}),
+          animate('500ms ease-in')
+        ]),
+      ])
+  ]
 })
 export class IceCreamTableComponent implements OnInit {
   dataSource: FlavorDataSource | null;
@@ -24,7 +41,6 @@ export class IceCreamTableComponent implements OnInit {
   }
 
   public voteFor(id: any) {
-    console.log(id);
     const ref = this.db.database.ref(`/flavors/${id}/votes`);
     ref.transaction((currentVotes) => currentVotes + 1);
   }
@@ -43,15 +59,15 @@ export class FlavorDataSource extends DataSource<any> {
   constructor(private db: AngularFireDatabase) {
     super();
     this.votesSubject = new Subject<any>();
-    this.votesSubject.forEach((x) => { console.log('Subject ', x);});
   }
 
   votesSubject: Subject<any>;
 
   /** Connect function called by the table to retrieve one stream containing the data to render. */
   connect(): Observable<FlavorData[]> {
-    return this.db.list('/flavors', {query: {orderByChild: 'votes'}})
+    const flavorList = this.db.list('/flavors', {query: {orderByChild: 'votes'}})
       .map((array) => array.reverse()) as FirebaseListObservable<FlavorData[]>;
+    return flavorList;
   }
 
   disconnect() {}
