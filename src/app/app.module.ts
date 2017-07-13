16ab8772e3ecc82e40d0736d6d62718c9b34514c
Michael Giambalvo <heathkit@google.com>
2017-07-13 04:00:15 -0700

    Basic table with static data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MdCoreModule, MdTableModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk';
 
 import { AppComponent } from './app.component';
 import { IceCreamTableComponent } from './ice-cream-table/ice-cream-table.component';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MdCoreModule,
+    MdTableModule,
+  ]
+})
+export class IceCreamMaterialModule {}
 
 @NgModule({
   declarations: [
@@ -12,6 +23,7 @@ import { IceCreamTableComponent } from './ice-cream-table/ice-cream-table.compon
   ],
   imports: [
     BrowserModule,
+    IceCreamMaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/ice-cream-table/ice-cream-table.component.ts
+++ src/app/ice-cream-table/ice-cream-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DataSource} from '@angular/cdk';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-ice-cream-table',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ice-cream-table.component.css']
 })
 export class IceCreamTableComponent implements OnInit {
+  dataSource: FlavorDataSource | null;
+  displayedColumns = ['votes', 'image', 'name'];
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = new FlavorDataSource();
   }
 
+}
+
+export interface FlavorData {
+  votes: number;
+  imageUrl: string;
+  name: string;
+}
+
+const FLAVORS: FlavorData[] = [
+  {votes: 20, imageUrl: '/assets/americone_dream.jpg', name: 'Americone Dream'},
+  {votes: 10, imageUrl: '/assets/rocky_road.jpg', name: 'Rocky Road'},
+];
+
+export class FlavorDataSource extends DataSource<any> {
+  dataChange: BehaviorSubject<FlavorData[]> = new BehaviorSubject<FlavorData[]>([]);
+
+  constructor() {
+    super();
+    this.dataChange.next(FLAVORS);
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<FlavorData[]> {
+    return this.dataChange;
+  }
+
+  disconnect() {}
 }
