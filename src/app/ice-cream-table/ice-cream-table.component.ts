d6ac940391ff64bcc8ff6118558cccd9c3735a27
Michael Giambalvo <heathkit@google.com>
2017-07-14 04:23:08 -0700

    Add workaround for material bug
    https://github.com/angular/material2/issues/5593
    There's a race condition that breaks md-table on route changes. Add a
    setTimeout so we trigger a second change detection after onInit.
    Also run format

--- src/app/ice-cream-table/ice-cream-table.component.ts
+++ src/app/ice-cream-table/ice-cream-table.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {DataSource} from '@angular/cdk';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Observable } from 'rxjs/Observable';
-import {GroupedObservable} from 'rxjs/operator/groupBy';
-import {Subscription} from 'rxjs/Subscription';
-import {Subject} from 'rxjs/Subject';
+import { DataSource } from '@angular/cdk';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { GroupedObservable } from 'rxjs/operator/groupBy';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import {
   trigger,
@@ -28,13 +28,13 @@ const VOTE_DELAY = 750;
   templateUrl: './ice-cream-table.component.html',
   styleUrls: ['./ice-cream-table.component.css'],
   animations: [
-      trigger('pulse', [
-        state('in', style({background: 'white'})),
-        transition('void => *', [
-          style({background: 'lightseagreen'}),
-          animate('350ms ease-out')
-        ]),
-      ])
+    trigger('pulse', [
+      state('in', style({ background: 'white' })),
+      transition('void => *', [
+        style({ background: 'lightseagreen' }),
+        animate('350ms ease-out')
+      ]),
+    ])
   ]
 })
 export class IceCreamTableComponent implements OnInit, OnDestroy {
@@ -42,14 +42,18 @@ export class IceCreamTableComponent implements OnInit, OnDestroy {
   displayedColumns = ['votes', 'image', 'name', 'like'];
   private vote = new Subject<any>();
   vote$ = this.vote.asObservable();
-  disabled: {[key: number]: boolean} = {};
+  disabled: { [key: number]: boolean } = {};
 
   voteSub: Subscription;
 
   constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
-    this.dataSource = new FlavorDataSource(this.db);
+    // Workaround for https://github.com/angular/material2/issues/5593
+    setTimeout(() => {
+      this.dataSource = new FlavorDataSource(this.db);
+    }, 1);
+
     this.voteSub = this.vote$.groupBy(s => s).subscribe((value: any) => {
       const id = value.key;
       value.throttleTime(VOTE_DELAY - 250).subscribe(() => {
@@ -82,8 +86,8 @@ export class FlavorDataSource extends DataSource<any> {
 
   /** Connect function called by the table to retrieve one stream containing the data to render. */
   connect(): Observable<FlavorData[]> {
-    const flavorList = this.db.list('/flavors', {query: {orderByChild: 'votes'}})
-      .map((array) => array.slice().reverse() ) as FirebaseListObservable<FlavorData[]>;
+    const flavorList = this.db.list('/flavors', { query: { orderByChild: 'votes' } })
+      .map((array) => array.slice().reverse()) as FirebaseListObservable<FlavorData[]>;
     return flavorList;
   }
 
