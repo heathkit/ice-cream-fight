e01155d3b71ab9d1d7e806c136ddeb331d7e6eb6
Michael Giambalvo <heathkit@google.com>
2017-07-13 17:33:21 -0700

    Add unit test for ice cream table.

--- src/app/ice-cream-table/ice-cream-table.component.spec.ts
+++ src/app/ice-cream-table/ice-cream-table.component.spec.ts
@@ -1,25 +1,83 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+import { IceCreamMaterialModule } from './../app.module';
+import { async, discardPeriodicTasks, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Subject } from 'rxjs/Subject';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { IceCreamTableComponent } from './ice-cream-table.component';
+import { IceCreamTableComponent, FlavorData } from './ice-cream-table.component';
+
+const STATIC_DATA = [
+  { 'votes': 20, 'imageUrl': '/assets/americone_dream.jpg', 'name': 'Americone Dream' },
+  { 'votes': 10, 'imageUrl': '/assets/rocky_road.jpg', 'name': 'Rocky Road' }
+];
 
 describe('IceCreamTableComponent', () => {
   let component: IceCreamTableComponent;
   let fixture: ComponentFixture<IceCreamTableComponent>;
 
+  const listSub = new Subject<any>();
+  const transactionSpy = jasmine.createSpy('transaction');
+  const listSpy = jasmine.createSpy('list').and.callFake(() => {
+    return listSub.asObservable();
+  });
+
+  const angularFireStub: any = {
+    list: listSpy,
+    database: {
+      ref: () => {
+        return { transaction: transactionSpy };
+      }
+    }
+  };
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IceCreamTableComponent ]
+      declarations: [IceCreamTableComponent],
+      providers: [
+        { provide: AngularFireDatabase, useValue: angularFireStub }
+      ],
+      imports: [IceCreamMaterialModule, NoopAnimationsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IceCreamTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    listSpy.calls.reset();
+    transactionSpy.calls.reset();
+    fixture.autoDetectChanges();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should display the flavors', () => {
+    listSub.next(STATIC_DATA);
+    fixture.detectChanges();
+
+    const rows = fixture.nativeElement.querySelectorAll('md-row');
+    expect(listSpy).toHaveBeenCalled();
+    expect(rows.length).toEqual(STATIC_DATA.length);
   });
+
+  it('should rate limit votes', fakeAsync(() => {
+    listSub.next(STATIC_DATA);
+    fixture.detectChanges();
+
+    const rows = fixture.nativeElement.querySelectorAll('md-row');
+    const vote = rows[0].querySelectorAll('button')[0];
+
+    vote.click();
+    expect(transactionSpy).toHaveBeenCalled();
+    transactionSpy.calls.reset();
+    tick(100);
+
+    vote.click();
+    expect(transactionSpy).not.toHaveBeenCalled();
+    tick(1000);
+
+    vote.click();
+    expect(transactionSpy).toHaveBeenCalled();
+    discardPeriodicTasks();
+  }));
 });
